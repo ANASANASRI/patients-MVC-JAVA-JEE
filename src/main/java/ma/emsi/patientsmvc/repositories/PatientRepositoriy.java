7ba4dc618ee379fbd0f4a0eb72b7ff75e04a0a99
ANAS ANASRI <100525138+ANASRIANAS@users.noreply.github.com>
2023-04-07 00:06:25 +0000

    third commit

--- src/main/java/ma/emsi/patientsmvc/repositories/PatientRepositoriy.java
+++ src/main/java/ma/emsi/patientsmvc/repositories/PatientRepositoriy.java
@@ -1,8 +1,10 @@
 package ma.emsi.patientsmvc.repositories;
 
 import ma.emsi.patientsmvc.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepositoriy extends JpaRepository<Patient,Long> {
-
+    Page<Patient> findByNomContains(String kw, Pageable pageable);
 }
--- src/main/java/ma/emsi/patientsmvc/web/PatientController.java
+++ src/main/java/ma/emsi/patientsmvc/web/PatientController.java
@@ -3,9 +3,12 @@ package ma.emsi.patientsmvc.web;
 import lombok.AllArgsConstructor;
 import ma.emsi.patientsmvc.entities.Patient;
 import ma.emsi.patientsmvc.repositories.PatientRepositoriy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,11 +17,24 @@ import java.util.List;
 public class PatientController {
     private PatientRepositoriy patientRepositoriy;
     @GetMapping(path = "/index")
-    public String patients(Model model) {
+    public String patients(Model model,
+                           @RequestParam(name ="page",defaultValue = "0") int page,
+                           @RequestParam(name ="size",defaultValue = "5") int size,
+                           @RequestParam(name ="keyword",defaultValue = "") String keyword) {
 
-        List<Patient> patients = patientRepositoriy.findAll();
-        model.addAttribute("listPatients",patients);
+        Page<Patient> pagePatients = patientRepositoriy.findByNomContains(keyword,PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("pageCurrent",page);
+        model.addAttribute("keyword",keyword);
         return "patients";
-
     }
+
+    @GetMapping(path = "/delete")
+    public String delete(long id) {
+        patientRepositoriy.deleteById(id);
+        return "redirect:/index";
+    }
+
+    //1h:16min
 }
